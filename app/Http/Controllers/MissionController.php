f796d36bc0c8854db358f0b5c00af22161455ee4
Omistusama <lattiassia@gmail.com>
2021-04-01 15:55:23 +0200

    commit

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -18,6 +18,13 @@ use PhpParser\Node\Expr\New_;
 
 class MissionController extends Controller
 {
+    function __construct()
+    {
+         $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
+         $this->middleware('permission:role-create', ['only' => ['create','store']]);
+         $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+    }
 
     /**
      * Display a listing of the resource.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="icon" type="icon" href="{{ asset('favicon.ico') }}">
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
@@ -27,6 +27,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
     </head>
     <body class="font-sans antialiased">
